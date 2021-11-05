c82949c6689e798e3851fd4dd246b03f43974e11
Ryan Mark <rymark907@gmail.com>
2021-11-05 11:43:23 -0700

    correct default completed values for projects and tasks

--- api/project/model.js
+++ api/project/model.js
@@ -2,11 +2,27 @@ const db = require('../../data/dbConfig')
 
 async function postProject(project) {
     const newProjectId = await db('projects').insert(project)
-    return db('projects').where('project_id', newProjectId).first()
+    const newProject = await db('projects').where('project_id', newProjectId).first()
+
+    if (newProject.project_completed === 0) {
+        newProject.project_completed = false
+    } else {
+        newProject.project_completed = true
+    }
+    return newProject
 }
 
-function getProjects() {
-    return db('projects')
+async function getProjects() {
+    const projects = await db('projects')
+    
+    projects.map(project => {
+        if (project.project_completed === 0) {
+            project.project_completed = false
+        } else {
+            project.project_completed = true
+        }
+    })
+    return projects
 }
 
 module.exports = {
--- api/task/model.js
+++ api/task/model.js
@@ -2,11 +2,27 @@ const db = require('../../data/dbConfig')
 
 async function postTask(task) {
     const newTaskId = await db('tasks').insert(task)
-    return db('tasks').where('task_id', newTaskId).first()
+    const newTask = await db('tasks').where('task_id', newTaskId).first()
+
+    if (newTask.task_completed === 0) {
+        newTask.task_completed = false
+    } else {
+        newTask.task_completed = true
+    }
+    return newTask
 }
 
-function getTasks() {
-    return db('tasks')
+async function getTasks() {
+    const tasks = await db('tasks')
+
+    tasks.map(task => {
+        if (task.task_completed === 0) {
+            task.task_completed = false
+        } else {
+            task.task_completed = true
+        }
+    })
+    return tasks
 }
 
 module.exports = {
